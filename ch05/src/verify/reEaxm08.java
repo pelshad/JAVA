0829fa4729979a99905e7f7f203fb5ccfa150dc7
micha <micha@192.168.219.106>
2022-10-30 19:36:59 +0900

    복습중

---
+++ ch05/src/verify/reEaxm08.java
@@ -0,0 +1,27 @@
+package verify;
+
+public class reEaxm08 {
+
+	public static void main(String[] args) {
+		int[][] array = {
+				{95, 86},
+				{83,92,96},
+				{78, 83, 93, 87, 88}
+		};
+	
+	int sum = 0;
+	int count = 0;
+	
+	for(int i = 0; i < array.length; i++) {
+		for(int j=0; j < array[i].length; j++) {
+			sum += array[i][j];
+			count++;
+		}
+	}
+	double avg = (double)sum/count;
+	
+	System.out.println("sum" + sum);
+	System.out.println("avg" + avg);
+	
+	}
+}
---
+++ ch05/src/verify/reEaxm09.java
@@ -0,0 +1,56 @@
+package verify;
+import java.util.Scanner;
+
+public class reEaxm09 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int selNum;
+		int stuNum = 0;
+		int[] exam = null;
+		int inExam;
+		int sumNum = 0;
+		double avgNum = 0.0;
+		int count = 0;
+		
+		while(true) {
+			System.out.println("-------------------------------------");
+			System.out.println("1.학생수|2.점수입력|3.점수리스트|4.분석|5.종료");
+			System.out.println("-------------------------------------");
+			System.out.print("선택>");
+			selNum = scan.nextInt();
+			if(selNum == 1) {
+				System.out.print("학생수 > ");
+				stuNum = scan.nextInt();
+				selNum = 0;
+				exam = new int[stuNum];
+			}
+			if(selNum == 2) {
+				for(int i = 0; i<exam.length ; i++) {
+					inExam = scan.nextInt();
+					exam[i] = inExam;
+				}
+			}
+			if(selNum == 3) {
+				for(int i = 0; i<exam.length; i++) {
+					System.out.println(exam[i]);
+				}
+			}
+			if(selNum == 4) {
+				for(int i =0; i<exam.length; i++) {
+					sumNum += exam[i];
+					count++;
+				}
+				System.out.println("최고 점수 : " + sumNum);
+				avgNum = (double)sumNum/count;
+				System.out.println("평균 점수 : " + avgNum);
+			}
+			if(selNum == 5) {
+				break;
+			}
+		
+		}
+		
+
+	}
+
+}
