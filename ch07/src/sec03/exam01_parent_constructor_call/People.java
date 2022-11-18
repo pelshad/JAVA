58ba56f4c6fa11d43d0d76aef92d62a34bf831dd
pelshad <akuaru@gmail.com>
2022-11-18 11:33:36 +0900

    metadata

---
+++ ch07/src/sec03/exam01_parent_constructor_call/People.java
@@ -0,0 +1,12 @@
+package sec03.exam01_parent_constructor_call;
+
+public class People {
+	public String name;
+	public String ssn;
+	
+	public People(String name, String ssn) {
+		this.name = name;
+		this.ssn = ssn;
+	}
+	//기본 생성자는 없는 상태
+}
