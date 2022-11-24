ad373d7c0987cae982113e58b2334916f99167a6
pelshad <akuaru@gmail.com>
2022-11-24 22:00:04 +0900

    8.3 인터페이스 구현(1)

---
+++ ch08/src/sec03/exam01_name_implement_class/RemoteControlExample.java
@@ -0,0 +1,19 @@
+package sec03.exam01_name_implement_class;
+
+public class RemoteControlExample {
+	public static void main(String[] args) {
+		//인터페이스 변수 선언
+		RemoteControl rc;
+		
+		//인터페이스는 본인을 대입할 수 없다.
+		//rc = new RemoteControl;
+		
+		//implements된 클래스의 객체를 생성
+		rc = new Television();
+		rc = new Audio();
+		
+		//개발코드
+		rc.turnOn();
+		rc.turnOff();
+	}
+}
---
+++ ch08/src/sec03/exam01_name_implement_class/Television.java
@@ -0,0 +1,37 @@
+package sec03.exam01_name_implement_class;
+
+public class Television implements RemoteControl{
+	//인터페이스에서 선언된 추상메소드를 오버라이딩하지 않으면 컴파일 에러
+	//이클립스에서 인터페이스의 추상메소드를 전부 호출하는 방법
+	//1. source -> override/implement Methods
+	//2. 에러가난 클래스명에 마우스를 올려 Add unimplemented methods 클릭
+
+	private int volume;
+	
+	
+	@Override
+	public void turnOn() {
+		System.out.println("TV를 켭니다.");
+	}
+
+	@Override
+	public void turnOff() {
+		System.out.println("TV를 끕니다.");
+	}
+
+	@Override
+	public void setVolume(int volume) {
+		//RemoteControl에서 MAX_VOLUME = 10, MIN_VOLUME = 0
+		//으로 선언되어 있으므로 분기를 작성
+		if(volume>RemoteControl.MAX_VOLUME) {
+			this.volume = RemoteControl.MAX_VOLUME;
+		} else if(volume<RemoteControl.MIN_VOLUME) {
+			this.volume = RemoteControl.MIN_VOLUME;
+		} else {
+			this.volume = volume;
+		}
+		System.out.println("현재 TV 볼륨 : " + this.volume);
+	}
+	
+
+}
