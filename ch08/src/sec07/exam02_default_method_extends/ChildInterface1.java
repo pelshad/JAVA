a7a25fbba0c1d64e77341c66e335678c8cd88769
pelshad <akuaru@gmail.com>
2022-12-01 21:53:55 +0900

    8.7 디폴트 메소드와 인터페이스 확장 - default 메소드 활용

--- ch08/src/sec07/exam02_default_method_extends/ChildInterface1.java
+++ ch08/src/sec07/exam02_default_method_extends/ChildInterface1.java
@@ -1,6 +1,6 @@
 package sec07.exam02_default_method_extends;
 
 public interface ChildInterface1 extends ParentInterface {
-	//부모의 default메소드를 재정의 하지 않음
+	//부모의 default메소드를 재정의 하지 않음//
 	public void method3();
 }
