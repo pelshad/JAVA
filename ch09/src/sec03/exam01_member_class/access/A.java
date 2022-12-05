88563f4c8014aabfd732d1393eb7c99581362781
pelshad <akuaru@gmail.com>
2022-12-05 20:47:31 +0900

    9.3 중첩 클래스의 접근 제한 - 인스턴스 클래스와 정적 클래스

---
+++ ch09/src/sec03/exam01_member_class/access/A.java
@@ -0,0 +1,32 @@
+package sec03.exam01_member_class.access;
+
+public class A {
+	class B {};
+	static class C {};
+	
+	//인스턴스 필드
+	//B와 C 전부 생성 가능
+	B field1 = new B();
+	C field2 = new C();
+	
+	//인스턴스 메소드
+	void method1() {
+	//둘다 로컬 변수화 가능
+		B var1 = new B();
+		C var2 = new C();
+	}
+	
+	//정적 필드
+	//A가 생성되지 않은 상태에서 인스턴스 객체인 B는 생성 불가
+	//static B field3 = new B();
+	////A가 생성되지 않은 상태지만 정적 객체인 C는 생성 가능
+	static C field4 = new C();
+	
+	//정적 메소드
+	static void method2() {
+		//A가 생성되지 않은 상태에서 인스턴스 로컬 객체인 B는 생성 불가
+		//B var3 = new B();
+		////A가 생성되지 않은 상태지만 정적 로컬 객체인 C는 생성 가능
+		C var4 = new C();
+	}
+}
