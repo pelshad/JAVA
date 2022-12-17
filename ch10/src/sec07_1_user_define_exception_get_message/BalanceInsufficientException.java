671f04859526e4c84b3ab0357ce719c8cc38b1b0
pelshad <akuaru@gmail.com>
2022-12-17 23:32:27 +0900

    10.8 예외 정보 얻기

--- ch10/src/sec07_1_user_define_exception_get_message/BalanceInsufficientException.java
+++ ch10/src/sec07_1_user_define_exception_get_message/BalanceInsufficientException.java
@@ -5,7 +5,7 @@ package sec07_1_user_define_exception_get_message;
 public class BalanceInsufficientException extends Exception {
 	//기본 생성자 생성
 	public BalanceInsufficientException() {};
-	//예외 발생시 보낼 오류문 작성
+	//예외 발생시 보낼 오류문 작성//
 	public BalanceInsufficientException(String message) {
 		super(message);
 	}
