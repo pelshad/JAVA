357b895c98dd1e334b9228822aff021015399f96
pelshad <akuaru@gmail.com>
2022-12-19 21:36:27 +0900

    11.3 Object 클래스(1) - equals()

--- ch11/src/sec03/exam01_equals/Member.java
+++ ch11/src/sec03/exam01_equals/Member.java
@@ -7,6 +7,9 @@ public class Member {
 	//하지만 보통 논리적 동등(객체가 서로 다르더라도 그 객체의 데이터가 같다면 같다고 보는것)
 	//을 위해 직접적으로 사용하기보단 오버라이딩하여 사용한다.
 	//대표적으로 String의 equals() 메소드
+	
+	//단,equlas() 메소드만으로 완전한 논리적 동등이라고 할 수는 없음
+	//다음장 hashCode()참고
 	public String id;
 	public Member(String id) {
 		this.id = id;
