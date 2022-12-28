a39d3a4d5bbfaa81a7ed18266fdf464d2dec2bf0
pelshad <akuaru@gmail.com>
2022-12-28 21:03:49 +0900

    11.5 System 클래스

---
+++ ch11/src/sec05/exam00_System/SystemIntro.java
@@ -0,0 +1,12 @@
+package sec05.exam00_System;
+
+public class SystemIntro {
+
+	public static Object out;
+	/*
+		System 클래스 용도
+		 1. 운영체제의 기능을 일부 이용 가능
+		  1) 프로그램 종료, 키보드로부터 입력, 모니터로 출력, 메모리 정리, 현재 시간 읽기
+		  2) 시스템 프로퍼티 읽기, 환경 변수 읽기
+	 */
+}
---
+++ ch11/src/sec05/exam01_exit/ExitExample.java
@@ -0,0 +1,28 @@
+package sec05.exam01_exit;
+
+public class ExitExample {
+	
+	public static void main(String[] args) {
+		//»ç¿ë ¾ÈµÊ
+		System.setSecurityManager(new SecurityManager() {
+			@Override
+			public void checkExit(int status) {
+				if(status!=5) {
+					throw new SecurityException();
+				}
+				
+			}
+		});
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i);
+			try {
+				System.exit(i);
+			} catch(SecurityException e) {
+				
+			}
+			
+		}
+	}
+}
