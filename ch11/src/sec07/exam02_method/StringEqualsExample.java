648608d2162ce426184e39de906f80481f3697b9
pelshad <akuaru@gmail.com>
2023-01-11 19:56:31 +0900

    11.7 String 클래스(1)

--- ch11/src/sec07/exam02_method/StringEqualsExample.java
+++ ch11/src/sec07/exam02_method/StringEqualsExample.java
@@ -3,19 +3,37 @@ package sec07.exam02_method;
 public class StringEqualsExample {
 
 	public static void main(String[] args) {
-		String ssn = "010604-1230123";
-		char sex = ssn.charAt(7);
-		switch(sex) {
-		case '1':
-		case '3':
-			System.out.println("남자입니다.");
-			break;
-		case '2':
-		case '4':
-			System.out.println("여자입니다.");
-			break;
+		String strVar1 = new String("홍길동");
+		String strVar2 = "홍길동";
+		String strVar3 = "홍길동";
+		
+		//new로 생성된 문자열 객체는 서로 다른 주소값을 가짐
+		if(strVar1 == strVar2) {
+			System.out.println("같은 String 객체를 참조");
+		} else {
+			System.out.println("다른 String 객체를 참조");
 		}
-
+		//선언형으로 만들어진 문자열 객체는 같은 주소값을 가짐
+		if(strVar2 == strVar3) {
+			System.out.println("같은 String 객체를 참조");
+		} else {
+			System.out.println("다른 String 객체를 참조");
+		}
+		
+		//equals는 object가 가진 메소드로 주소값을 참조하여 true/false를 리턴하나
+		//String타입에서 override되어 문자열 자체를 비교하여 true/false를 리턴한다.
+		if(strVar1.equals(strVar2)) {
+			System.out.println("같은 문자열임");
+		} else {
+			System.out.println("다른 문자열임");
+		}
+		
+		if(strVar2.equals(strVar3)) {
+			System.out.println("같은 문자열임");
+		} else {
+			System.out.println("다른 문자열임");
+		}
+		
 	}
 
 }
