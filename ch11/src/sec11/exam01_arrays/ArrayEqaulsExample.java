4168697a7c643eb812cc097ea7ea2e2ec0b02746
pelshad <akuaru@gmail.com>
2023-01-20 21:38:43 +0900

    aa

---
+++ ch11/src/sec11/exam01_arrays/ArrayEqaulsExample.java
@@ -0,0 +1,17 @@
+package sec11.exam01_arrays;
+
+import java.util.Arrays;
+
+public class ArrayEqaulsExample {
+
+	public static void main(String[] args) {
+		int[][] origin= {{1,2},{1,4}};
+		
+		//얕은 복사(equals) : 참조값 비교
+		int[][] clone1 = Arrays.copyOf(origin, origin.length);
+		System.out.println("배열 번지 비교 : " + origin.equals(clone1));//
+		
+		
+	}
+
+}
